15511dd4197518ce54999fc7f561f1bd7e6768d7
Christopher Brown <cbrown@decisionpatterns.com>
2015-01-25 07:06:11 -0800

    N.B. ?canCoerce

---
+++ R/can.R
@@ -0,0 +1,30 @@
+#' Determine if the a vector can be coerced into the given clas
+#' 
+#' @param object object; 
+#' @param class character; name of class to check if it can be coerced
+#' 
+#' @seealso 
+#'   \code{\link[methods]{canCoerce}}
+#'   \code{\link[methods]{setAs}}
+#'   
+#' @examples 
+#'   can( 1.0, 'integer' )
+#'   can( "foo", 'integer' )
+#'       
+#' @return logical 
+#' @export
+
+can <- function( object, class ) {
+  show.errors <- getOption("show.error.messages")
+  if( show.errors == TRUE )
+    options( show.error.messages=FALSE )
+    
+  try( {cancan <- as(object,class)}, silent=TRUE )
+  if( show.errors == TRUE )
+    options( show.error.messages=show.errors )
+  
+  ! is.na(cancan)
+  
+}
+
+
