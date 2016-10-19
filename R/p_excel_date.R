34882e8f77512a1099d818dede7253988169b0b6
Christopher Brown <cbrown@decisionpatterns.com>
2016-10-19 04:52:47 -0700

    Make p_excel_dt an S3 method

--- R/p_excel_date.R
+++ R/p_excel_date.R
@@ -26,8 +26,19 @@ excel_origin <- structure(-25569, class = "Date") # "1899-12-30"
 #'    
 #' @examples
 #'   p_excel_dt( 40000 )
-#'     
+#'   p_excel_dt( c(40000,NA)) 
+#'   p_excel_dt( Sys.Date() )
+#'   p_excel_dt( Sys.time() )   
+#'   p_excel_dt( c(Sus)) 
 #' @import lubridate
 #' @export 
 
-p_excel_dt <- function(x) excel_origin + lubridate::ddays(x)
+p_excel_dt <- function(x) UseMethod('p_excel_dt') 
+
+p_excel_dt.default <- function(x) p_excel_dt(as.numeric(x))
+
+p_excel_dt.numeric <- function(x) excel_origin + lubridate::ddays(x)
+
+p_excel_dt.Date <- function(x) x
+
+p_excel_dt.POSIXct <- function(x) x 
