2ae4d2dda2c4ec4bf6103e622d362ff38f9fe503
Christopher Brown <cbrown@decisionpatterns.com>
2017-01-09 15:46:28 -0800

    v0.2.3: mv p_currency -> p_dollar

--- R/p_dollar.R
+++ R/p_dollar.R
@@ -2,14 +2,19 @@
 #' 
 #' @param x character; text currency values
 #' 
-#' Parses a character represented currency value to numeric value
+#' Parses a character represented numeric USD value by removing \code{$} and 
+#' \code{","} values.
 #' 
+#' @seealso 
+#'   \code{scales::dollar} which is the inverse operations that expresses a 
+#'   numeric value as a scalar
+#'   
 #' @examples
 #'   factor("$1,000.20") %>% from_currency
 #' 
 #' @export
 
-p_currency <- function(x) {
+p_dollar <- function(x) {
   if( is.factor(x) ) x <- as.character(x)
   if( is.character(x) )
     x <- x %>% str_replace("^[$]", "" )  %>% str_replace_all(",","") %>% as.numeric
